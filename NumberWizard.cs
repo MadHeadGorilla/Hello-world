66690fab5433db0b36326bd88cd401d8b55a1c11
MadHeadGorilla <48532290+MadHeadGorilla@users.noreply.github.com>
2019-03-23 16:19:29 +0200

    Add files via upload

---
+++ NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+
+    }
+    void StartGame ()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Добро пожаловать в мир магии чисел");
+        Debug.Log("Выбери число");
+        Debug.Log("Ты можешь выбрать не больше: " + max);
+        Debug.Log("Ты можешь выбрать не менее: " + min);
+        Debug.Log("Скажи пожалуйста твоё число меньше или больше чем" + guess);
+        Debug.Log("Нажми вверх = больше, вниз = меньше, нажми Enter = правильно");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();   
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Поздравляю ты стал медовой плюшечкой, ням ням ням");
+            StartGame();
+        }
+    
+        void NextGuess()
+        {
+            guess = (max + min) / 2;
+            Debug.Log("Твое число выше или ниже чем " + guess);
+        }
+    }
+
+}
